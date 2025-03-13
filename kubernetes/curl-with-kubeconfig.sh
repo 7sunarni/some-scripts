f3e836c60522191cab359dba0c55ad9d8f55fd59
7sunarni <710720732@qq.com>
2025-03-13 16:31:39 +0800

    Update curl-with-kubeconfig.sh

--- kubernetes/curl-with-kubeconfig.sh
+++ kubernetes/curl-with-kubeconfig.sh
@@ -1,8 +1,11 @@
 # !/bin/bash
-cat $KUBECONFIG | grep "client-certificate-data:" | awk -F ' ' '{print $2}' | base64 -d > /tmp/client-certificate
-cat $KUBECONFIG | grep "client-key-data:" | awk -F ' ' '{print $2}' | base64 -d > /tmp/client-key-data
-cat $KUBECONFIG | grep "certificate-authority-data:" | awk -F ' ' '{print $2}'  | base64 -d > /tmp/certificate-authority-data
-EP=$(cat $KUBECONFIG | grep "server:" | awk -F ' ' '{print $2}')
+if [ -z $KUBECONFIG ]; then
+        _KUBECONFIG=~/.kube/config
+fi
+cat $_KUBECONFIG | grep "client-certificate-data:" | awk -F ' ' '{print $2}' | base64 -d > /tmp/client-certificate
+cat $_KUBECONFIG | grep "client-key-data:" | awk -F ' ' '{print $2}' | base64 -d > /tmp/client-key-data
+cat $_KUBECONFIG | grep "certificate-authority-data:" | awk -F ' ' '{print $2}'  | base64 -d > /tmp/certificate-authority-data
+export EP=$(cat $_KUBECONFIG | grep "server:" | awk -F ' ' '{print $2}')
 
 alias kurl='curl -k --cacert "/tmp/certificate-authority-data" --cert "/tmp/client-certificate"  --key "/tmp/client-key-data"'
 
