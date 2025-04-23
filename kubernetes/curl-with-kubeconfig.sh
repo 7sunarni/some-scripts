641aaf1a80c6026c9ab21df08f8f012ffa9d72b1
7sunarni <710720732@qq.com>
2025-04-23 09:33:57 +0800

    Update curl-with-kubeconfig.sh

--- kubernetes/curl-with-kubeconfig.sh
+++ kubernetes/curl-with-kubeconfig.sh
@@ -1,7 +1,10 @@
 # !/bin/bash
 if [ -z $KUBECONFIG ]; then
         _KUBECONFIG=~/.kube/config
+else
+        _KUBECONFIG=$KUBECONFIG
 fi
+
 cat $_KUBECONFIG | grep "client-certificate-data:" | awk -F ' ' '{print $2}' | base64 -d > /tmp/client-certificate
 cat $_KUBECONFIG | grep "client-key-data:" | awk -F ' ' '{print $2}' | base64 -d > /tmp/client-key-data
 cat $_KUBECONFIG | grep "certificate-authority-data:" | awk -F ' ' '{print $2}'  | base64 -d > /tmp/certificate-authority-data
